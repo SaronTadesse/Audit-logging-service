1361eac2bad4aa4eb60337183ccbe83f52353171
sarontadesse <sarontadesse456@gmail.com>
2024-07-20 22:07:34 -0500

    initial commit

---
+++ src/main/java/saron_audit_logging_service/SaronAuditLoggingServiceApplication.java
@@ -0,0 +1,13 @@
+package saron_audit_logging_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaronAuditLoggingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaronAuditLoggingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/saron_audit_logging_service/SaronAuditLoggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package saron_audit_logging_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaronAuditLoggingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
